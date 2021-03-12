dd7f263eea19782614d7e3afaa7d55e0ac5972a9
Jiabin Huang <j.huang@uke.de>
2021-03-12 16:05:17 +0100

    Update 3_generate_hipath.pl

--- 3_generate_hipath.pl
+++ 3_generate_hipath.pl
@@ -1,3 +1,7 @@
+
+
+
+
 #!/usr/bin/perl
 use strict;
 use warnings;
@@ -11,14 +15,17 @@ use threads::shared;
 #use String::Util 'trim';
 
 # function of this file: calculate the hipath from ss-file.
- 
+
 my $data_file=$ARGV[0]."/"."files_to_be_processed.txt";
 open(DAT, $data_file) || die("Could not open file!");
-my @raw_data=<DAT>; 
+my @raw_data=<DAT>;
 close(DAT);
+
+
 
 print "Starting main program\n";
 my @childs;
+my @commands;
 my $wrestler;
 foreach $wrestler (@raw_data)
 {
@@ -29,7 +36,7 @@ foreach $wrestler (@raw_data)
 
     my $path_directory = $ARGV[0]."/".$rootname.'.hipaths';
     if ( !-d $path_directory ) {
-	make_path $path_directory or die "Failed to create path: $path_directory";
+        make_path $path_directory or die "Failed to create path: $path_directory";
     }
 
     my $filename = $ARGV[0]."/".$rootname.'.kin';
@@ -42,8 +49,8 @@ foreach $wrestler (@raw_data)
     close FILE;
     print "The number of lines in $filename is $line_number.\n";
 
-    open my $file, '<', $filename; 
-    my $first_line = <$file>; 
+    open my $file, '<', $filename;
+    my $first_line = <$file>;
     close $file;
     #$first_line = trim($first_line);
     $first_line =~ s/^\s+|\s+$//g ;
@@ -55,33 +62,64 @@ foreach $wrestler (@raw_data)
     my $i=0;
     for ($i=0;$i<$line_number;$i++)
     {
-	my $pid = fork();
-	if($pid){
-		push(@childs, $pid);
-	}elsif ($pid == 0){
-	    my $j=0;
-	    for ($j=0;$j<$line_number;$j++)
-	    {
-                print "RNAliHiPath -f ${ARGV[0]}/${rootname}.aln -k ${kvalue} -F ${ARGV[0]}/${rootname}.ssfiles/${i}_${j}.ss -t 1 > $path_directory/${i}_${j}.hipath\n";
-		unless (-e "$path_directory/${i}_${j}.hipath") {
-		    system "RNAliHiPath -f ${ARGV[0]}/${rootname}.aln -k ${kvalue} -F ${ARGV[0]}/${rootname}.ssfiles/${i}_${j}.ss -t 1 > $path_directory/${i}_${j}.hipath";
-		}
-	    }
-	    exit 0;
-	}else{
-	    die "couldnt fork: $!\n";
-	}
-	#$num++;
+            #my $pid = fork();
+            #if($pid){
+            #   push(@childs, $pid);
+            #}elsif ($pid == 0){
+            # the targets in a pid
+            my $j=0;
+            for ($j=0;$j<$line_number;$j++)
+            {
+                unless (-e "$path_directory/${i}_${j}.hipath") {
+                    push(@commands, "RNAliHiPath -f ${ARGV[0]}/${rootname}.aln -k ${kvalue} -F ${ARGV[0]}/${rootname}.ssfiles/${i}_${j}.ss -t 1 > $path_directory/${i}_${j}.hipath");
+                #    system "~/bin/RNAlihishapes/src/RNAlihipath -f ${ARGV[0]}/${rootname}.aln -k ${kvalue} -F ${ARGV[0]}/${rootname}.ssfiles/${i}_${j}.ss -t 1 > $path_directory/${i}_${j}.hipath";
+                }
+            }
+            #exit 0;
+            #}else{
+            #die "couldnt fork: $!\n";
+            #}
+        #$num++;
     }
 }
 
 
- 
-print "waitpid\n";
-foreach (@childs) {
-        my $tmp = waitpid($_, 0);
-        print "done with pid $tmp\n";
+
+#foreach (@commands) {
+#  print "$_\n";
+#}
+use POSIX;
+my $length = scalar @commands;
+print "$length\n";
+my $ceil = ceil(@commands / 176);
+
+
+
+for (my $z = 0; $z < @commands; $z += $ceil) {
+
+    # take a slice of 4 elements from @files
+    my @subcomm = @commands[$z .. $z + $ceil-1];
+
+    # do something with them in a child process
+    if (fork() == 0) {
+        #system @subcomm;
+        my $length2 = scalar @subcomm;
+        print $length2;
+        foreach (@subcomm) {
+            system "$_";
+        }
+        exit;   # <--- this is very important
+    }
 }
- 
+# wait for the child processes to finish
+print @commands/$ceil;
+wait for 0 .. @commands/$ceil;
+
+
+#print "waitpid\n";
+#foreach (@childs) {
+#        my $tmp = waitpid($_, 0);
+#        print "done with pid $tmp\n";
+#}
+
 print "End hipath calculation\n";
- 
